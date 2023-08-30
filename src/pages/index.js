b93752270031a4724be9d9f0769f1f3182265ddc
CookedIdeas <113336281+CookedIdeas@users.noreply.github.com>
2023-08-30 14:41:28 +0200

    adding analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import Skills from '../3_components/3_homepage/Skills';
 import Contact from '../3_components/3_homepage/Contact';
 import Header from '../3_components/2_layouts/1_navigation/Header';
 import { useState } from 'react';
+import { Script } from 'gatsby';
 
 const IndexPage = () => {
   const { dark, closeNavHeader } = useGlobalContext();
@@ -60,6 +61,8 @@ const IndexPage = () => {
       className={`${dark ? 'dark' : ''} bg-background-light
         dark:bg-background-dark h-full`}
     >
+      <Script id="abla-analytics-script">{` !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('phc_jmD6XPHBZKL8DUebf8ruzOOnAyFU6SGUFDm3jKPE8tR',{api_host:'https://e.abla.io'})`}</Script>
       <Hero />
 
       <div id="index_content" className="relative">
